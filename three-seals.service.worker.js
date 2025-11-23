26c8a8a08e99822b7461f8264998e09c561a60f5
frostbyte-dev-0b01 <frostbyte.dev.0b01@gmail.com>
2025-11-23 14:26:53 -0500

    Update including win screen

--- three-seals.service.worker.js
+++ three-seals.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762474114|830274845';
+const CACHE_VERSION = '1763925832|17626578038';
 /** @type {string} */
 const CACHE_PREFIX = '3 Seals-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
