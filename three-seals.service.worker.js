cd6d8d7cb93adc61049385e0d595b7678b31b01c
frostbyte-dev-0b01 <frostbyte.dev.0b01@gmail.com>
2025-11-23 21:08:41 -0500

    Just skip the js stuff

--- three-seals.service.worker.js
+++ three-seals.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763948928|5457313514';
+const CACHE_VERSION = '1763949456|5984708742';
 /** @type {string} */
 const CACHE_PREFIX = 'three-seals-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
