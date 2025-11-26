c3fe2734b70587b7cebeead815fdf1201c5cc6d9
frostbyte-dev-0b01 <frostbyte.dev.0b01@gmail.com>
2025-11-26 12:31:23 -0500

    rename everything to index and update to latest

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,20 +4,20 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763949456|5984708742';
+const CACHE_VERSION = '1764178215|144641233697';
 /** @type {string} */
 const CACHE_PREFIX = 'three-seals-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
-const OFFLINE_URL = 'three-seals.offline.html';
+const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
 const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 // Files that will be cached on load.
 /** @type {string[]} */
-const CACHED_FILES = ["three-seals.html","three-seals.js","three-seals.offline.html","three-seals.icon.png","three-seals.apple-touch-icon.png","three-seals.audio.worklet.js","three-seals.audio.position.worklet.js"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["three-seals.wasm","three-seals.pck"];
+const CACHEABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
