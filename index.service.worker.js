15fe931c06324ca5a66bc35d58960d01e6d1895d
frostbyte-dev-0b01 <frostbyte.dev.0b01@gmail.com>
2026-02-02 21:32:34 -0500

    Test browser save/reload functionality

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770078220|247277458';
+const CACHE_VERSION = '1770085797|9403494038';
 /** @type {string} */
 const CACHE_PREFIX = 'Runes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
