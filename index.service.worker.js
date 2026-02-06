66993227e0eece59de61e63e0897fa7981f26c49
frostbyte-dev-0b01 <frostbyte.dev.0b01@gmail.com>
2026-02-05 21:38:23 -0500

    testing v0.0.1

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770167005|465763942';
+const CACHE_VERSION = '1770345308|4207065';
 /** @type {string} */
 const CACHE_PREFIX = 'Runes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
