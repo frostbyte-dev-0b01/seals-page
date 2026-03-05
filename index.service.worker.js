6ce92e9b821f95fd5bd9eee6bcfd47f87a0a7e72
frostbyte-dev-0b01 <frostbyte.dev.0b01@gmail.com>
2026-03-04 19:04:51 -0500

    remove version stubs

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772668640|3235004';
+const CACHE_VERSION = '1772669075|3261146';
 /** @type {string} */
 const CACHE_PREFIX = 'Runes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
