b898db1dc98594e646e378a125e93e2ba492c109
resident-ogre <danmoramarco@gmail.com>
2026-03-10 20:27:49 -0400

    testing emoji

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773179746|3345861';
+const CACHE_VERSION = '1773188735|3732368';
 /** @type {string} */
 const CACHE_PREFIX = 'Runes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
