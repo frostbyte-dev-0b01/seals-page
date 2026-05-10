975cd8b4ced68c0379733a8162a2ab740721007a
resident-ogre <danmoramarco@gmail.com>
2026-05-10 17:50:23 -0400

    phone testing 1.1.2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778356794|3806788';
+const CACHE_VERSION = '1778449745|3736990';
 /** @type {string} */
 const CACHE_PREFIX = 'Runes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
